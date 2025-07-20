834861485d957962a04046c1cc1146d84ac81abc
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-07-21 01:42:46 +0300

    Update index.ts

--- packages/@byteform/core/src/index.ts
+++ packages/@byteform/core/src/index.ts
@@ -1,8 +1,21 @@
 import "./style.css";
 
 export * from "./_theme";
-export * from "./_utils";
 
 export * from "./Avatar";
+export * from "./Badge";
 export * from "./Button";
+export * from "./Card";
+export * from "./Checkbox";
+export * from "./ColorInput";
+export * from "./ColorPicker";
+export * from "./ColorSwatch";
+export * from "./IconButton";
+export * from "./Input";
 export * from "./Loader";
+export * from "./Paper";
+export * from "./PasswordInput";
+export * from "./Progress";
+export * from "./Select";
+export * from "./Switch";
+export * from "./Transition";
