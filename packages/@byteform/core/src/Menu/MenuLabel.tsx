569991f296e97876e15c06a68270bd97a2823020
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-07-23 03:28:13 +0300

    New component Menu

---
+++ packages/@byteform/core/src/Menu/MenuLabel.tsx
@@ -0,0 +1,41 @@
+import { useTheme } from "../_theme";
+import { useMenu } from "./context";
+import { MenuLabelProps } from "./types";
+
+const getPosition = (position: MenuLabelProps["position"]) => {
+    switch (position) {
+        case "right":
+            return "text-right";
+        case "center":
+            return "text-center";
+        default:
+            return "text-left";
+    }
+};
+
+export const MenuLabel = ({
+    children,
+    className,
+    position = "left"
+}: MenuLabelProps) => {
+    const { theme, cx } = useTheme();
+    const { classNames } = useMenu();
+
+    return (
+        <div
+            className={cx(
+                "text-xs font-semibold px-3 pt-2 outline-none",
+                theme === "light"
+                    ? "text-[var(--byteform-light-hint)]"
+                    : "text-[var(--byteform-dark-hint)]",
+                getPosition(position),
+                className,
+                classNames?.label
+            )}
+        >
+            {children}
+        </div>
+    );
+};
+
+MenuLabel.displayName = "@byteform/core/Menu.Label";
