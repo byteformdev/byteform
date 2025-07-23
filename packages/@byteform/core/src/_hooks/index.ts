47cbe15bfcd6901d6a3a698e071f213e4b649369
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-07-23 03:27:38 +0300

    Update index.ts

--- packages/@byteform/core/src/_hooks/index.ts
+++ packages/@byteform/core/src/_hooks/index.ts
@@ -1,3 +1,5 @@
+export * from "./use-counter";
 export * from "./use-disclosure";
-export * from "./use-scroll-into-view";
 export * from "./use-hover";
+export * from "./use-scroll-into-view";
+export * from "./use-selection";
