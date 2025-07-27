fe9c3e03181119c3dd71f0505449c82a3acf02bd
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-07-27 14:10:04 +0300

    Update index.ts

--- packages/@byteform/core/src/index.ts
+++ packages/@byteform/core/src/index.ts
@@ -11,7 +11,6 @@ export * from "./Badge";
 export * from "./Breadcrumbs";
 export * from "./Button";
 export * from "./Card";
-export * from "./Carousel";
 export * from "./Checkbox";
 export * from "./Code";
 export * from "./ColorInput";
