b5b1011dae40fcf2c750224b729f13add8f59d2f
Eeka Kivivuori <shilliwowi@gmail.com>
2025-08-27 10:15:01 +0300

    Added h and w full

--- packages/@byteform/core/src/Center/Center.tsx
+++ packages/@byteform/core/src/Center/Center.tsx
@@ -3,37 +3,37 @@ import { CenterProps } from "./types";
 import { useTheme } from "../_theme";
 
 export const Center = forwardRef<HTMLDivElement, CenterProps>(
-    (
-        {
-            children,
-            inline = false,
-            component: Component,
-            absolute = false,
-            className,
-            ...props
-        },
-        ref
-    ) => {
-        const { cx } = useTheme();
+  (
+    {
+      children,
+      inline = false,
+      component: Component,
+      absolute = false,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const { cx } = useTheme();
 
-        const Element = Component || "div";
+    const Element = Component || "div";
 
-        return (
-            <Element
-                ref={ref}
-                className={cx(
-                    "flex justify-center items-center",
-                    inline && "inline-flex",
-                    absolute &&
-                        "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
-                    className
-                )}
-                {...props}
-            >
-                {children}
-            </Element>
-        );
-    }
+    return (
+      <Element
+        ref={ref}
+        className={cx(
+          "flex h-full w-full justify-center items-center",
+          inline && "inline-flex",
+          absolute &&
+            "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </Element>
+    );
+  }
 );
 
-Center.displayName = "Center";
+Center.displayName = "@byteform/core/Center";
