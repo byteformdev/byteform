c5178bf2f3d7f78d9ea3386b98e96ccd6e0ed320
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-09-13 14:48:41 +0300

    indexes

--- packages/@byteform/core/src/components/index.ts
+++ packages/@byteform/core/src/components/index.ts
@@ -1,1 +1,3 @@
+export * from "./Button";
 export * from "./For";
+export * from "./Loader";
--- packages/@byteform/core/src/index.ts
+++ packages/@byteform/core/src/index.ts
@@ -1,3 +1,6 @@
+import "./style.css";
+
+export * from "./types/index";
 export * from "./utils/index";
 export * from "./theme/index";
 export * from "./components/index";
